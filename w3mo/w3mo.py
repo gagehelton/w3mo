00f68cc90f0f498b8fcda3b590033a6eb962d97d
gageh@slintegrated.com <michaelhelton@Michaels-MacBook-Pro.local>
2020-04-10 21:52:00 -0400

    update cmd usage

--- w3mo/w3mo.py
+++ w3mo/w3mo.py
@@ -1,5 +1,6 @@
 import sys
 import requests
+import xml.etree.ElementTree as ET
 
 class _DEFAULTS():
     headers={
@@ -11,25 +12,36 @@ class _DEFAULTS():
     
     actions = {"GET_STATE":"GetBinaryState",
                     "SET_STATE":"SetBinaryState",
-                    "GET_NAME":"GetFriendlyName"}
+                    "GET_NAME":"GetFriendlyName",
+                    "SET_NAME":"Name"}
 
-    states = {"BINARY_STATE":"BinaryState"}
+    states = {"STATE":"BinaryState","NAME":"FriendlyName"}
     
     port = 49153
-
+    timeout = 10
     
-
     base_url = 'http://{device}:{port}/upnp/control/basicevent1'.format(device = "{device}",port=port)
 
-    xml_body = '''
+    post_xml = '''
         <?xml version="1.0" encoding="utf-8"?>
         <s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/"
                 s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
             <s:Body>
                 <u:{action} xmlns:u="urn:Belkin:service:basicevent:1">
                     <{state}>{value}</{state}>
-                </u:{action}
-            ></s:Body>
+                </u:{action}>
+            </s:Body>
+        </s:Envelope>
+    '''
+    
+    get_xml = '''
+        <?xml version="1.0" encoding="utf-8"?>
+        <s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/"
+                s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
+            <s:Body>
+                <u:{action} xmlns:u="urn:Belkin:service:basicevent:1">
+                </u:{action}>
+            </s:Body>
         </s:Envelope>
     '''
 
@@ -46,35 +58,56 @@ class w3mo():
         if(parse_kwargs(required,kwargs)):
             self.ip = kwargs['ip']
             self.url = _DEFAULTS.base_url.format(device=self.ip)
-            
+
+    def parse_xml(self,value,search):
+        xml = ET.fromstring(value)
+        for node in xml.iter(search):
+            if(node.text):
+                return node.text
+            else:
+                print("something is off... \n\n {} \n\n".format(node))
+                return False
+    
     def control(self,**kwargs):
         required = {"action":{"type":str},"state":{"type":str},"value":{"type":int}}
         if(parse_kwargs(required,kwargs)):
-            headers = _DEFAULTS.headers
+            headers = _DEFAULTS.headers.copy()
             headers['SOAPACTION'] = headers['SOAPACTION'].format(**kwargs)
 
-            data = _DEFAULTS.xml_body.format(**kwargs)
+            data = _DEFAULTS.post_xml.format(**kwargs)
 
-            print("{}\n{}\n{}\n\n\n\n".format(self.url,headers,data))
+            #print("{}\n{}\n{}\n\n\n\n".format(self.url,headers,data))
 
-            response = requests.post(self.url,headers=headers,data=data)
-            print(response)
-            print(response.text)
+            response = requests.post(self.url,headers=headers,data=data,timeout=_DEFAULTS.timeout)
+            if(response.status_code == 200):
+                state = self.parse_xml(response.text,_DEFAULTS.states['STATE'])
+                return state
 
     #works!
     def get(self,**kwargs):
-        required = {"action":{"type":str},"state":{"type":str},"value":{"type":int}}
+        required = {"action":{"type":str}}
         if(parse_kwargs(required,kwargs)):
-            headers = _DEFAULTS.headers
-            headers['SOAPACTION'] = headers['SOAPACTION'].format(**kwargs)
+            try:
+                headers = _DEFAULTS.headers.copy()
+                headers['SOAPACTION'] = headers['SOAPACTION'].format(**kwargs)
+    
+                data = _DEFAULTS.get_xml.format(**kwargs)
+    
+                #print("{}\n{}\n{}\n\n\n\n".format(self.url,headers,data))
+    
+                response = requests.get(self.url,headers=headers,data=data,timeout=_DEFAULTS.timeout)
 
-            data = _DEFAULTS.xml_body.format(**kwargs)
-
-            print("{}\n{}\n{}\n\n\n\n".format(self.url,headers,data))
-
-            response = requests.get(self.url,headers=headers,data=data)
-            print(response)
-            print(response.text)
+                if(response.status_code == 200):
+                    if('name' in action.lower()):
+                        search = _DEFAULTS.states['NAME']
+                    elif('state' in action.lower()):
+                        search = _DEFAULTS.states['STATE']
+                    else:
+                        return False
+                    value = self.parse_xml(response.text,search)
+                    return value
+            except Exception as e:
+                print(str(e))
 
 if __name__ == '__main__':
 
@@ -88,37 +121,58 @@ if __name__ == '__main__':
     except Exception as e:
         print(str(e))
 
+    print("\nWelcome to W3mo Pwn!\nTo control your device just enter a state integer (0=OFF,1=ON)\nTo get your device power state enter 'state'\nTo get your device's friendly name enter 'name'\nHappy controlling!\n")
+
     while True:
         try:
-            value = input("Please Enter The Desired State (0=OFF,1=ON): ")
+            
+            value = input('\nw3mo.sh3ll$ ')
+            response = False
             try:
                 value = int(value)
             except:
                 if(isinstance(value,str) and value == 'exit'):
                     break
-                print("Entry Not An Integer!\n")
-                error_counter += 1
-                value = False   
 
-            '''
-            if(not isinstance(value,bool)):         
-                x.control(
+            if(not isinstance(value,bool) and isinstance(value,int)):         
+                response = x.control(
                     action=_DEFAULTS.actions['SET_STATE'],
-                    state=_DEFAULTS.states['BINARY_STATE'],
+                    state=_DEFAULTS.states['STATE'],
+                    value=value
+                    )
+                if(response):
+                    if(response == '1'):
+                        current_state = 'ON'
+                    else:
+                        current_state = 'OFF'
+                    print("Success! Your device is: {}\n".format(current_state))
+                else:
+                    print("Failure!\n")
+                error_counter = 0
+
+            elif(not isinstance(value,bool) and isinstance(value,str)):   
+                if('name' in value.lower()):
+                    action = _DEFAULTS.actions['GET_NAME']
+                elif('state' in value.lower()):
+                    action = _DEFAULTS.actions['GET_STATE']      
+                response = x.get(
+                    action=action,
                     value=value
                 )
+                if(response):
+                    if(response == '1'):
+                        current_state = 'ON'
+                    elif(isinstance(value,int) or 'state' in value.lower()):
+                        current_state = 'OFF'
+                    else:
+                        current_state = response
+                    print("Success! The data you seek is: {}\n".format(current_state))
+                else:
+                    print("Failure!\n")
                 error_counter = 0
-            '''
-            if(not isinstance(value,bool)):         
-                x.get(
-                    action=_DEFAULTS.actions['GET_STATE'],
-                    state=_DEFAULTS.states['BINARY_STATE'],
-                    value=value
-                )
-                error_counter = 0
-                
+
             else:
-                print("no value...")
+                print("no usable value...")
                 error_counter += 1
 
         except Exception as e:
@@ -128,6 +182,7 @@ if __name__ == '__main__':
         if(error_counter >= 5):
             print("Terminating...")
             break
+
 
 
 
