4eddfcf703f1d7556f5b68d5d8c149d868046604
Gage Helton <ghelton@sudotronics.com>
2020-12-11 03:02:21 +0000

    make names lowercase! very important release

--- w3mo/w3mo.py
+++ w3mo/w3mo.py
@@ -118,7 +118,7 @@ class w3mo():
                 action=_DEFAULTS.actions['GET_NAME'],
                 value='name'
             )
-            self.name = response
+            self.name = response.lower()
         except Exception as e:
             if(debug): print("{} | {}".format(type(e).__name__,e.args))
             
